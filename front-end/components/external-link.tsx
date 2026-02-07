0adf7c064062066f7136f20e9b0d439959be89a5
Moulik Gupta <moulik08@gmail.com>
2026-02-07 09:34:35 -0500

    Created the Expo Website Project (front-end folder)

---
+++ front-end/components/external-link.tsx
@@ -0,0 +1,29 @@
+// @ts-ignore
+import { Href, Link as NativeLink } from 'expo-router';
+
+// Cast Link to any to fix strict TS error "JSX element class does not support attributes"
+const Link = NativeLink as any;
+import { openBrowserAsync, WebBrowserPresentationStyle } from 'expo-web-browser';
+import { type ComponentProps } from 'react';
+
+type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: Href & string };
+
+export function ExternalLink({ href, ...rest }: Props) {
+  return (
+    <Link
+      target="_blank"
+      {...rest}
+      href={href}
+      onPress={async (event: any) => {
+        if (process.env.EXPO_OS !== 'web') {
+          // Prevent the default behavior of linking to the default browser on native.
+          event.preventDefault();
+          // Open the link in an in-app browser.
+          await openBrowserAsync(href, {
+            presentationStyle: WebBrowserPresentationStyle.AUTOMATIC,
+          });
+        }
+      }}
+    />
+  );
+}
